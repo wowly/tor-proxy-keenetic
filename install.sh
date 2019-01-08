ab337b906fc118d92f413596fe3931e8c852ccef
MonakhovK <46240852+MonakhovK@users.noreply.github.com>
2019-01-08 17:47:36 +0600

    beta 0.3
    small fixes

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
-# pause for debuging
-pause ()
-{
-echo "To continue hit RETURN"
-read q
-}
 
 echo "Installation started"
-echo "Installing the necessary packages from Entware repository"
-#pause
+echo "\nInstalling the necessary packages from Entware repository"
 opkg update
 opkg install ipset dnsmasq-full iptables tor tor-geoip bind-dig cron mc
 echo "Packages was intalled"
 pause
 
 echo "Start copying config files"
-#pause
 #Tor config file
 cp -f ./conf/torrc /opt/etc/tor/
 #List of nodes for redirect to tor
@@ -23,10 +15,8 @@ cp -f ./conf/proxylist.txt /opt/etc/
 #Dnsmasq config
 cp -f ./conf/dnsmasq.conf /opt/etc/
 echo "Configs was intalled"
-pause
 
-echo "Start copying scripts to startup"
-#pause
+echo "Start copying scripts"
 cp ./bin/100-ipset.sh /opt/etc/ndm/fs.d/
 chmod +x /opt/etc/ndm/fs.d/100-ipset.sh
 
@@ -47,11 +37,8 @@ cp ./bin/S99proxylist /opt/etc/init.d/
 chmod +x /opt/etc/init.d/S99proxylist
 
 echo "Scripts was intalled"
-pause
 
 echo "Adding task to crontab"
-#pause
 echo -e "\n00 01 * * * root /opt/bin/proxylist_ipset.sh" >> /opt/etc/crontab
 echo "Task was added"
-pause
 echo -e "Installation completed.\nNow you need to connect to the router using telnet(port 23) or ssh (port 21) and enter the following commands:\n opkg dns-override \n system configuration save \n system reboot \n"
